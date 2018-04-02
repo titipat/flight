6a42a35f91ebd696d05740e0a72a7b7094b23159
Titipat <ttpsvb@gmail.com>
2018-04-02 14:27:14 +0700

    Add request handling

--- index.php
+++ index.php
@@ -8,13 +8,26 @@ Flight::route('/', function(){
 });
 
 Flight::route('/mysql-example', function () {
-    $db = Flight::db();
-    $statement = $db->query('SELECT * FROM employees;');
+    $statement = Flight::db()->query('SELECT * FROM employees;');
     $statement->setFetchMode(PDO::FETCH_OBJ);
     echo "employee_id,first_name,last_name<br>";
     while($row = $statement->fetch()) {
         echo "{$row->employee_id},{$row->first_name},{$row->last_name}<br>";
     }
+});
+
+Flight::route('GET /example', function () {
+    $username = Flight::request()->query->username;
+    echo "Hi, this is a GET method. You have sent a request as {$username}.";
+});
+
+Flight::route('GET /example/@username', function ($username) {
+    echo "Hi, this is a GET method. You have sent a request as {$username}.";
+});
+
+Flight::route('POST /example', function () {
+    $username = Flight::request()->data->username;
+    echo "Hi, this is a POST method. You have sent a request as {$username}.";
 });
 
 Flight::start();
